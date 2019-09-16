bb7113ba95c6e13f4b765a8ed5d35ee171961023
priyansh <priynash.rajput@logiclewings.com>
2019-09-16 22:09:56 +0530

    init Commit

---
+++ app/src/main/java/com/example/expansecalculatorapp/activities/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.expansecalculatorapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected void initData() {
+
+    }
+
+    protected void initUi() {
+
+    }
+}
---
+++ app/src/main/java/com/example/expansecalculatorapp/fragments/BaseFragment.java
@@ -0,0 +1,14 @@
+package com.example.expansecalculatorapp.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+    protected void initData() {
+
+    }
+
+    protected void initUi() {
+
+    }
+}
